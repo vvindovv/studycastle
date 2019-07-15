5f97f8158a148de237b8919f10648b7f42547004
sangen <tkddjs5@naver.com>
2019-07-15 14:50:04 +0900

    views

--- scapp/views.py
+++ scapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+import scapp.views
+# Create your views here.
+
 def index(request):
-    return request(request, 'html')
+    return render(request, 'index.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def blogsingle(request):
+    return render(request, 'blog-single.html')
